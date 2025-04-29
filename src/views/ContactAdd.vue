192a9f06af69204735072495cf7cf0036d93ea5a
Chung Ket <ketdc23v7x105@dttx.ctu.edu.vn>
2025-04-29 23:22:07 +0700

    tạo trang thêm mới liên hệ

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,36 +1,38 @@
+<!-- trang them moi lien he -->
 <template>
-    <div v-if="contact" class="page">
-        <h4>Thêm Liên hệ mới</h4>
-        <ContactForm :contact="contact" @submit:contact="createContact" />
-        <p>{{ message }}</p>
-    </div>
+  <div v-if="contact" class="page">
+    <h4>Thêm Liên hệ mới</h4>
+    <ContactForm :contact="contact" @submit:contact="createContact" />
+    <p>{{ message }}</p>
+  </div>
 </template>
 <script>
 import ContactForm from "@/components/ContactForm.vue";
 import ContactService from "@/services/contact.service";
 export default {
-    components: {
-        ContactForm,
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contact: {},
+      message: "",
+    };
+  },
+
+  methods: {
+    async createContact(data) {
+      try {
+        await ContactService.create(data);
+        alert("Liên hệ được tạo thành công.");
+        this.$router.push({ name: "contactbook" });
+      } catch (error) {
+        console.log(error);
+      }
     },
-    data() {
-        return {
-            contact: {},
-            message: "",
-        };
-    },
-    methods: {
-        async createContact(data) {
-            try {
-                await ContactService.create(data);
-                alert('Liên hệ được tạo thành công.');
-                this.$router.push({ name: "contactbook" });
-            } catch (error) {
-                console.log(error);
-            }
-        },
-    },
-    created() {
-        this.message = "";
-    },
+  },
+  created() {
+    this.message = "";
+  },
 };
 </script>
